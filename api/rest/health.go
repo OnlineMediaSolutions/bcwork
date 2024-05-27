997268be8a86e6dcdce0dc86661c4ef3203a43ed
ifinkiel <ifinkiel@gmail.com>
2024-05-27 17:19:37 +0300

    Fixing DeployProcess

---
+++ api/rest/health.go
@@ -0,0 +1,17 @@
+package rest
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"net/http"
+)
+
+// @Description Check Health of Service
+// @Tags Health
+// @Accept json
+// @Produce html
+// @Router /ping [get]
+func PingPong(c *fiber.Ctx) error {
+
+	return c.Status(http.StatusOK).JSON(Response{Status: "OK", Message: "Service is UP!!!"})
+
+}
