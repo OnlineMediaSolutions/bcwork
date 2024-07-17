55cc8df97a0fd3800d8fe6d962ae3950b758fb21
sonaisrayel <sonai@brightcom.com>
2024-07-17 12:29:35 +0400

    Add constant values

---
+++ utils/constant/browser.go
@@ -0,0 +1,15 @@
+package constant
+
+var AllowedBrowsers = map[string]bool{
+	"safari":          true,
+	"chrome":          true,
+	"firefox":         true,
+	"opera":           true,
+	"edge":            true,
+	"facebook-app":    true,
+	"iphone":          true,
+	"samsung-browser": true,
+	"pinterest":       true,
+	"medal":           true,
+	"other":           true,
+}
