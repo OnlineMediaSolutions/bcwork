fe6ff13c878d86e6d3a485c1004c95b22e1d3686
Mateo Fucci <mateof@onlinemediasolutions.com>
2024-07-24 17:10:09 +0200

    factors - push factor_changes to postgres, improved logging, refactoring

---
+++ migrations/009_factor.up.sql
@@ -0,0 +1,19 @@
+CREATE TABLE factor_changes (
+                                time TIMESTAMP NOT NULL,
+                                eval_time TIMESTAMP NOT NULL,
+                                pubimps INTEGER NOT NULL,
+                                soldimps INTEGER NOT NULL,
+                                cost FLOAT NOT NULL,
+                                revenue FLOAT NOT NULL,
+                                gp FLOAT NOT NULL,
+                                gpp FLOAT NOT NULL,
+                                publisher VARCHAR(10) NOT NULL,
+                                domain VARCHAR(255) NOT NULL,
+                                country CHAR(2) NOT NULL,
+                                device VARCHAR(50) NOT NULL,
+                                old_factor FLOAT NOT NULL,
+                                new_factor FLOAT NOT NULL,
+                                response_status INTEGER NOT NULL,
+                                increase FLOAT NOT NULL,
+                                PRIMARY KEY (publisher, domain, country, device, time)
+);
--- workers/factors/worker.go
+++ workers/factors/worker.go
@@ -6,10 +6,13 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/friendsofgo/errors"
+	"github.com/m6yf/bcwork/bcdb"
 	"github.com/m6yf/bcwork/config"
+	"github.com/m6yf/bcwork/models"
 	"github.com/m6yf/bcwork/quest"
 	"github.com/m6yf/bcwork/utils/bccron"
 	"github.com/rs/zerolog/log"
+	"github.com/volatiletech/sqlboiler/v4/boil"
 	"github.com/volatiletech/sqlboiler/v4/queries"
 	"io"
 	"math"
@@ -18,22 +21,20 @@ import (
 )
 
 type Worker struct {
-	Sleep          time.Duration `json:"sleep"`
-	DatabaseEnv    string        `json:"dbenv"`
-	Cron           string        `json:"cron"`
-	MaxGPP         float64       `json:"max_gpp"`
-	MinGPP         float64       `json:"min_gpp"`
-	TrendThreshold float64       `json:"trend_threshold"`
-	FactorStep     float64       `json:"factor_step"`
-	LowFactor      float64       `json:"low_factor"`
-	MinFactor      float64       `json:"min_factor"`
-	Domains        []string      `json:"domains"`
-	FilterExists   bool          `json:"filter_exists"`
+	Sleep        time.Duration `json:"sleep"`
+	DatabaseEnv  string        `json:"dbenv"`
+	Cron         string        `json:"cron"`
+	Domains      []string      `json:"domains"`
+	FilterExists bool          `json:"filter_exists"`
 }
 
 type FactorChanges struct {
 	Time       time.Time `json:"time"`
-	EvalTime   time.Time `json:"t1"`
+	EvalTime   time.Time `json:"eval_time"`
+	Pubimps    int       `json:"pubimps"`
+	Soldimps   int       `json:"soldimps"`
+	Revenue    float64   `json:"revenue"`
+	Cost       float64   `json:"cost"`
 	GP         float64   `json:"gp"`
 	GPP        float64   `json:"gpp"`
 	Publisher  string    `json:"publisher"`
@@ -45,26 +46,6 @@ type FactorChanges struct {
 	RespStatus int       `json:"response_status"`
 }
 
-//type FactorChanges struct {
-//	Time       time.Time `json:"time"`
-//	T1         time.Time `json:"t1"`
-//	T2         time.Time `json:"t2"`
-//	T1GP       float64   `json:"t1_gp"`
-//	T2GP       float64   `json:"t2_gp"`
-//	T2GPP      float64   `json:"t2_gpp"`
-//	Publisher  string    `json:"publisher"`
-//	Domain     string    `json:"domain"`
-//	Country    string    `json:"country"`
-//	Device     string    `json:"device"`
-//	OldFactor  float64   `json:"old_factor"`
-//	NewFactor  float64   `json:"new_factor"`
-//	RespStatus float64   `json:"response_status"`
-//}
-
-func (record *FactorChanges) Key() string {
-	return fmt.Sprintf("%s - %s - %s - %s", record.Publisher, record.Domain, record.Country, record.Device)
-}
-
 type FactorReport struct {
 	Time                 time.Time `boil:"time" json:"time" toml:"time" yaml:"time"`
 	PublisherID          string    `boil:"publisher_id" json:"publisher_id" toml:"publisher_id" yaml:"publisher_id"`
@@ -74,18 +55,24 @@ type FactorReport struct {
 	Revenue              float64   `boil:"revenue" json:"revenue" toml:"revenue" yaml:"revenue"`
 	Cost                 float64   `boil:"cost" json:"cost" toml:"cost" yaml:"cost"`
 	DemandPartnerFee     float64   `boil:"demand_partner_fee" json:"demand_partner_fee" toml:"demand_partner_fee" yaml:"demand_partner_fee"`
-	SoldImpressions      int64     `boil:"sold_impressions" json:"sold_impressions" toml:"sold_impressions" yaml:"sold_impressions"`
-	PublisherImpressions int64     `boil:"publisher_impressions" json:"publisher_impressions" toml:"publisher_impressions" yaml:"publisher_impressions"`
+	SoldImpressions      int       `boil:"sold_impressions" json:"sold_impressions" toml:"sold_impressions" yaml:"sold_impressions"`
+	PublisherImpressions int       `boil:"publisher_impressions" json:"publisher_impressions" toml:"publisher_impressions" yaml:"publisher_impressions"`
 	DataFee              float64   `boil:"data_fee" json:"data_fee" toml:"data_fee" yaml:"data_fee"`
 	Gp                   float64   `boil:"gp" json:"gp" toml:"gp" yaml:"gp"`
 	Gpp                  float64   `boil:"gpp" json:"gpp" toml:"gpp" yaml:"gpp"`
 }
+
 type Factor struct {
 	Publisher string  `boil:"publisher" json:"publisher" toml:"publisher" yaml:"publisher"`
 	Domain    string  `boil:"domain" json:"domain" toml:"domain" yaml:"domain"`
 	Device    string  `boil:"device" json:"device" toml:"device" yaml:"device"`
 	Factor    float64 `boil:"factor" json:"factor" toml:"factor" yaml:"factor"`
 	Country   string  `boil:"country" json:"country" toml:"country" yaml:"country"`
+}
+
+// Key functions for each struct
+func (record *FactorChanges) Key() string {
+	return fmt.Sprintf("%s - %s - %s - %s", record.Publisher, record.Domain, record.Country, record.Device)
 }
 
 func (rec *Factor) Key() string {
@@ -102,6 +89,7 @@ func (rec *FactorReport) CalculateGP() {
 	rec.Gp = roundFloat(Gp)
 }
 
+// Worker functions
 func (w *Worker) Init(ctx context.Context, conf config.StringMap) error {
 	var err error
 
@@ -110,32 +98,13 @@ func (w *Worker) Init(ctx context.Context, conf config.StringMap) error {
 		return errors.Wrapf(err, "failed to initalize DB")
 	}
 
-	w.Cron, _ = conf.GetStringValue("cron")
+	w.DatabaseEnv = conf.GetStringValueWithDefault("dbenv", "local_prod")
+	err = bcdb.InitDB(w.DatabaseEnv)
+	if err != nil {
+		return errors.Wrapf(err, "failed to initalize DB")
+	}
 
-	//w.MaxGPP, err = conf.GetFloat64ValueWithDefault("max_gpp", 0.65)
-	//if err != nil {
-	//	log.Warn().Err(err).Msg("failed to fetch MaxGPP config value (will use default 0.65)")
-	//}
-	//w.MinGPP, err = conf.GetFloat64ValueWithDefault("min_gpp", 0.3)
-	//if err != nil {
-	//	log.Warn().Err(err).Msg("failed to fetch MinGPP config value (will use default 0.3)")
-	//}
-	//w.TrendThreshold, err = conf.GetFloat64ValueWithDefault("trend_threshold", 0.1)
-	//if err != nil {
-	//	log.Warn().Err(err).Msg("failed to fetch TrendThreshold config value (will use default 0.1)")
-	//}
-	//w.FactorStep, err = conf.GetFloat64ValueWithDefault("factor_step", 1.1)
-	//if err != nil {
-	//	log.Warn().Err(err).Msg("failed to fetch FactorStep config value (will use default 1.1)")
-	//}
-	//w.LowFactor, err = conf.GetFloat64ValueWithDefault("low_factor", 0.75)
-	//if err != nil {
-	//	log.Warn().Err(err).Msg("failed to fetch FactorStep config value (will use default 1.1)")
-	//}
-	//w.MinFactor, err = conf.GetFloat64ValueWithDefault("min_factor", 0.01)
-	//if err != nil {
-	//	log.Warn().Err(err).Msg("failed to fetch FactorStep config value (will use default 1.1)")
-	//}
+	w.Cron, _ = conf.GetStringValue("cron")
 
 	w.Domains, w.FilterExists = conf.GetStringSlice("domains", ",")
 	if !w.FilterExists {
@@ -150,7 +119,6 @@ func (w *Worker) Do(ctx context.Context) error {
 
 	var RecordsMap map[string]*FactorReport
 	var factors map[string]*Factor
-
 	var err error
 
 	start, end := generateTimes(30)
@@ -162,7 +130,7 @@ func (w *Worker) Do(ctx context.Context) error {
 	}
 
 	log.Info().Msg("fetch records from Factors API")
-	factors, err = FetchFactors()
+	factors, err = w.FetchFactors()
 	if err != nil {
 		return err
 	}
@@ -188,10 +156,19 @@ func (w *Worker) Do(ctx context.Context) error {
 		updatedFactor, err = w.CalculateFactor(record, oldFactor)
 		if err != nil {
 			log.Err(err).Msg("failed to calculate factor")
+			logJSON, err := json.Marshal(record)
+			if err != nil {
+				log.Err(err).Msg("failed to parse record to json.")
+			}
+			log.Info().Msg(fmt.Sprintf("%s", logJSON))
 		}
 		newFactors[key] = FactorChanges{
 			Time:      end,
 			EvalTime:  start,
+			Pubimps:   record.PublisherImpressions,
+			Soldimps:  record.SoldImpressions,
+			Cost:      record.Cost + record.DataFee + record.DemandPartnerFee,
+			Revenue:   record.Revenue,
 			GP:        record.Gp,
 			GPP:       record.Gpp,
 			Publisher: factors[key].Publisher,
@@ -204,19 +181,28 @@ func (w *Worker) Do(ctx context.Context) error {
 	}
 
 	for _, r := range newFactors {
-
 		if r.NewFactor != r.OldFactor {
 			err := r.updateFactor()
 			if err != nil {
 				return errors.Wrapf(err, fmt.Sprintf("Error Updating factor for key: Publisher=%s, Domain=%s, Country=%s, Device=%s. ResponseStatus: %d", r.Publisher, r.Domain, r.Country, r.Device, r.RespStatus))
 			}
 		}
-		logJSON, err := json.Marshal(r)
+
+		logJSON, err := json.Marshal(r) //Create log json to log it
 		if err != nil {
 			log.Info().Msg(fmt.Sprintf("Error marshalling log for key:%v entry: %v", r.Key(), err))
 		}
 		log.Info().Msg(fmt.Sprintf("%s", logJSON))
 
+		mod, err := r.ToModel()
+		if err != nil {
+			log.Error().Err(err).Msg("failed to convert to model")
+		}
+
+		err = mod.Upsert(ctx, bcdb.DB(), true, Columns, boil.Infer(), boil.Infer())
+		if err != nil {
+			log.Error().Err(err).Msg("failed to push log to postgres")
+		}
 	}
 
 	return nil
@@ -229,6 +215,7 @@ func (w *Worker) GetSleep() int {
 	return 0
 }
 
+// Fetch performance data from quest
 func FetchFromQuest(ctx context.Context, start time.Time, stop time.Time) (map[string]*FactorReport, error) {
 	var records []*FactorReport
 
@@ -269,6 +256,7 @@ GROUP BY 1, 2, 3, 4, 5`, startString, startString, stopString)
 	return RecordsMap, nil
 }
 
+// Update a factor via the API
 func (record *FactorChanges) updateFactor() error {
 	requestBody := map[string]interface{}{
 		"publisher": record.Publisher,
@@ -304,13 +292,19 @@ func (record *FactorChanges) updateFactor() error {
 	return nil
 }
 
-func FetchFactors() (map[string]*Factor, error) {
+// Fetch the factors from the Factors API
+func (w *Worker) FetchFactors() (map[string]*Factor, error) {
 	// Create the request body using a map
 	requestBody := map[string]interface{}{
 		"pagination": map[string]interface{}{
 			"page":      0,
-			"page_size": 3000,
+			"page_size": 10000,
 		}}
+
+	if w.FilterExists {
+		requestBody["filter"] = map[string]interface{}{"domain": w.Domains}
+	}
+	fmt.Println(requestBody)
 
 	// Marshal the request body to JSON
 	jsonData, err := json.Marshal(requestBody)
@@ -349,13 +343,29 @@ func FetchFactors() (map[string]*Factor, error) {
 	return factorsMap, nil
 }
 
-func generateTimes(minutes int) (time.Time, time.Time) {
-	end := time.Now().UTC().Truncate(time.Duration(minutes) * time.Minute)
-	start := end.Add(-time.Duration(minutes) * time.Minute)
+func (record *FactorChanges) ToModel() (models.FactorChange, error) {
+	model := models.FactorChange{
+		Time:           record.Time,
+		EvalTime:       record.EvalTime,
+		Pubimps:        record.Pubimps,
+		Soldimps:       record.Soldimps,
+		Cost:           roundFloat(record.Cost),
+		Revenue:        roundFloat(record.Revenue),
+		GP:             record.GP,
+		GPP:            record.GPP,
+		Publisher:      record.Publisher,
+		Domain:         record.Domain,
+		Country:        record.Country,
+		Device:         record.Device,
+		OldFactor:      record.OldFactor,
+		NewFactor:      record.NewFactor,
+		ResponseStatus: record.RespStatus,
+		Increase:       roundFloat((record.NewFactor / record.OldFactor) - 1)}
 
-	return start, end
+	return model, nil
 }
 
+// Helper functions
 func (w *Worker) CheckDomain(targetDomain string) bool {
 	if len(w.Domains) == 0 {
 		return true
@@ -400,87 +410,17 @@ func roundFloat(value float64) float64 {
 	return math.Round(value*100) / 100
 }
 
-//func (w *Worker) CalculateFactor(T2record *FactorReport, T1record *FactorReport, oldFactor float64) (float64, error) {
-//	var updatedFactor float64
-//
-//	if T2record == nil || T1record == nil {
-//		return oldFactor, errors.New("T2record or T1record cannot be nil")
-//	}
-//
-//	if T2record.Gpp > w.MaxGPP { //IF above Max GP (0.65) => Increase factor
-//		updatedFactor = oldFactor * w.FactorStep
-//	} else if T2record.Gpp < w.MinGPP && T2record.Gpp > 0 { //IF above Min GP (0.30) => Reduce factor
-//		updatedFactor = oldFactor / w.FactorStep
-//	} else if T2record.Gp < 0 && T1record.Gp > 0 { //IF T2 < 0 AND T1 > 0 => Set to Default low (0.5)
-//		updatedFactor = w.LowFactor
-//	} else if T2record.Gp > 0 && T1record.Gp < 0 { //IF T2 > 0 AND T1 < 0 => Keep Factor
-//		updatedFactor = oldFactor
-//	} else if T2record.Gp < 0 && T1record.Gp < 0 { //IF T2 < 0 AND T1 < 0 => Set to Default Min (0.01)
-//		updatedFactor = w.MinFactor
-//	} else if ((T2record.Gp - T1record.Gp) / T2record.Gp) >= w.TrendThreshold { //IF within thresholds & GP$ Positive trend => Increase factor
-//		updatedFactor = oldFactor * w.FactorStep
-//	} else if ((T2record.Gp - T1record.Gp) / T2record.Gp) <= (-1 * w.TrendThreshold) { // IF within thresholds & GP$ Negative trend => Decrease factor
-//		updatedFactor = oldFactor / w.FactorStep
-//	} else if (((T2record.Gp - T1record.Gp) / T2record.Gp) <= w.TrendThreshold) && ((T2record.Gp-T1record.Gp)/T2record.Gp) >= (-1*w.TrendThreshold) {
-//		return oldFactor, nil
-//	} else {
-//		return oldFactor, errors.New(fmt.Sprintf("unable to calculate factor: no matching condition Key: %s", T1record.Key()))
-//	}
-//
-//	return updatedFactor, nil
-//}
+func generateTimes(minutes int) (time.Time, time.Time) {
+	end := time.Now().UTC().Truncate(time.Duration(minutes) * time.Minute)
+	start := end.Add(-time.Duration(minutes) * time.Minute)
 
-//func FetchFromQuest(ctx context.Context, start time.Time, middle time.Time, stop time.Time) (map[string]*FactorReport, map[string]*FactorReport, error) {
-//	var records []*FactorReport
-//
-//	startString := start.Format("2006-01-02T15:04:05Z")
-//	middleString := middle.Format("2006-01-02T15:04:05Z")
-//	stopString := stop.Format("2006-01-02T15:04:05Z")
-//
-//	q := fmt.Sprintf(`SELECT CASE WHEN timestamp < '%s' THEN to_date('%s','yyyy-MM-ddTHH:mm:ssZ')  ELSE to_date('%s','yyyy-MM-ddTHH:mm:ssZ') END time,
-//       publisher publisher_id,
-//       domain,
-//       country,
-//       dtype device_type,
-//       sum(dbpr)/1000 revenue,
-//       sum(sbpr)/1000 cost,
-//       sum(dpfee)/1000 demand_partner_fee,
-//       count(1) sold_impressions,
-//       sum(CASE WHEN loop=false THEN 1 ELSE 0 END) publisher_impressions,
-//       sum(CASE WHEN uidsrc='iiq' THEN dbpr/1000 ELSE 0 END) * 0.045 data_fee
-//FROM impression
-//WHERE timestamp >= '%s'
-//  AND timestamp < '%s'
-//  AND publisher IS NOT NULL
-//  AND domain IS NOT NULL
-//  AND country IS NOT NULL
-//  AND dtype IS NOT NULL
-//GROUP BY 1, 2, 3, 4, 5`, middleString, startString, middleString, startString, stopString)
-//	log.Info().Str("q", q).Msg("processImpressionsCounters")
-//	err := queries.Raw(q).Bind(ctx, quest.DB(), &records)
-//	if err != nil {
-//		return nil, nil, errors.Wrapf(err, "failed to query impressions from questdb")
-//	}
-//
-//	var T1RecordsMap = make(map[string]*FactorReport)
-//	var T2RecordsMap = make(map[string]*FactorReport)
-//
-//	for _, r := range records {
-//		r.CalculateGP()
-//		if r.Time.Equal(start) {
-//			T1RecordsMap[r.Key()] = r
-//		} else if r.Time.Equal(middle) {
-//			T2RecordsMap[r.Key()] = r
-//		}
-//	}
-//
-//	return T1RecordsMap, T2RecordsMap, nil
-//}
+	return start, end
+}
 
-//func generateTimes(minutes int) (time.Time, time.Time, time.Time) {
-//	end := time.Now().UTC().Truncate(time.Duration(minutes) * time.Minute)
-//	middle := end.Add(-time.Duration(minutes) * time.Minute)
-//	start := end.Add(-2 * time.Duration(minutes) * time.Minute)
-//
-//	return start, middle, end
-//}
+var Columns = []string{
+	models.FactorChangeColumns.Time,
+	models.FactorChangeColumns.Publisher,
+	models.FactorChangeColumns.Domain,
+	models.FactorChangeColumns.Country,
+	models.FactorChangeColumns.Device,
+}
