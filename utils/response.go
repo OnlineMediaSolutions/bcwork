12e9ed3112f0f6f6e38b1adc56f88123277347ac
sonaisrayel <sonai@brightcom.com>
2024-08-06 16:12:31 +0400

    Fix dpo reponse message

--- utils/response.go
+++ utils/response.go
@@ -7,6 +7,7 @@ import (
 type Response struct {
 	Status  string `json:"status"`
 	Message string `json:"message"`
+	RuleId  string `json:"rule_id"`
 }
 
 func ErrorResponse(c *fiber.Ctx, statusCode int, message string) error {
@@ -20,6 +21,15 @@ func ErrorResponse(c *fiber.Ctx, statusCode int, message string) error {
 func SuccessResponse(c *fiber.Ctx, statusCode int, message string) error {
 	resp := Response{
 		Status:  "success",
+		Message: message,
+	}
+	return c.Status(statusCode).JSON(resp)
+}
+
+func CustomSuccessResponse(c *fiber.Ctx, statusCode int, ruleId string, message string) error {
+	resp := Response{
+		Status:  "success",
+		RuleId:  ruleId,
 		Message: message,
 	}
 	return c.Status(statusCode).JSON(resp)
