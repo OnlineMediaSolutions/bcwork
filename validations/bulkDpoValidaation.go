5ac0314fce1b19f14340fbde5e46bb23c79ca6d2
sonaisrayel <sonai@brightcom.com>
2024-08-06 15:54:58 +0400

    Add validations

--- validations/bulkDpoValidaation.go
+++ validations/bulkDpoValidaation.go
@@ -8,44 +8,46 @@ import (
 	"github.com/m6yf/bcwork/utils/constant"
 )
 
-func ValidateDPOBulk(c *fiber.Ctx) error {
-	var dpoList []core.Dpo
-	err := c.BodyParser(&dpoList)
+func ValidateDPOInBulk(c *fiber.Ctx) error {
+	var bulkRequest []core.Dpo
+	err := c.BodyParser(&bulkRequest)
 	if err != nil {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
 			"status":  "error",
-			"message": "Invalid request body for dpo. Please ensure it's a valid JSON array.",
+			"message": "Invalid request body for dpo. Please ensure it's a valid JSON.",
 		})
 	}
 
 	var errorMessages = map[string]string{
 		"country":   "Country code must be 2 characters long and should be in the allowed list",
-		"factorDpo": fmt.Sprintf("Factor value not allowed, it should be >= %d and <= %d", constant.MinDPOFactorValue, constant.MaxDPOFactorValue),
+		"factorDpo": fmt.Sprintf("Factor value not allowed, it should be >= %s and <= %s", fmt.Sprintf("%d", constant.MinDPOFactorValue), fmt.Sprintf("%d", constant.MaxDPOFactorValue)),
 	}
 
-	var validationErrors []fiber.Map
+	var validationErrors []map[string]string
 
-	for i, body := range dpoList {
-		err = Validator.Struct(body)
+	for _, body := range bulkRequest {
+		err := Validator.Struct(body)
 		if err != nil {
+			errorResponse := map[string]string{
+				"status": "error",
+			}
 			for _, err := range err.(validator.ValidationErrors) {
-				errorMessage := errorMessages[err.Tag()]
-				if errorMessage == "" {
-					errorMessage = fmt.Sprintf("%s is mandatory, validation failed", err.Field())
+				if msg, ok := errorMessages[err.Tag()]; ok {
+					errorResponse["message"] = msg
+				} else {
+					errorResponse["message"] = fmt.Sprintf("%s is mandatory, validation failed", err.Field())
 				}
-				validationErrors = append(validationErrors, fiber.Map{
-					"status":  "error",
-					"index":   i,
-					"field":   err.Field(),
-					"message": errorMessage,
-				})
 				break
 			}
+			validationErrors = append(validationErrors, errorResponse)
 		}
 	}
 
 	if len(validationErrors) > 0 {
-		return c.Status(fiber.StatusBadRequest).JSON(validationErrors)
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"status":   "error",
+			"messages": validationErrors,
+		})
 	}
 
 	return c.Next()
