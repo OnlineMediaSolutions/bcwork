5675e5d652703f4cda555ad6aac6ee2e65eb5642
sonaisrayel <sonai@brightcom.com>
2024-08-22 17:46:18 +0400

    Delete confiant code

--- core/confiant.go
+++ core/confiant.go
@@ -12,9 +12,11 @@ import (
 	"github.com/m6yf/bcwork/bcdb/qmods"
 	"github.com/m6yf/bcwork/models"
 	"github.com/m6yf/bcwork/utils"
+	"github.com/m6yf/bcwork/utils/bcguid"
 	"github.com/rotisserie/eris"
 	"github.com/volatiletech/sqlboiler/v4/boil"
 	"github.com/volatiletech/sqlboiler/v4/queries/qm"
+	"github.com/volatiletech/sqlboiler/v4/types"
 	"time"
 )
 
@@ -54,17 +56,6 @@ type ConfiantFilter struct {
 	Domain      filter.StringArrayFilter `json:"domain,omitempty"`
 	Rate        filter.StringArrayFilter `json:"rate,omitempty"`
 }
-
-type ConfiantRealtimeRecord struct {
-	Rule       string  `json:"rule"`
-	Rate       float64 `json:"rate"`
-	ConfiantId string  `json:"confiant_id"`
-}
-
-func (f ConfiantUpdateRequest) GetPublisher() string { return f.Publisher }
-func (f ConfiantUpdateRequest) GetDomain() string    { return f.Domain }
-func (c ConfiantUpdateRequest) GetDevice() string    { return "" }
-func (c ConfiantUpdateRequest) GetCountry() string   { return "" }
 
 func (confiant *Confiant) FromModel(mod *models.Confiant) error {
 
@@ -142,16 +133,44 @@ func (filter *ConfiantFilter) QueryMod() qmods.QueryModsSlice {
 
 func UpdateMetaDataQueue(c *fiber.Ctx, data *ConfiantUpdateRequest) error {
 
-	_, err := json.Marshal(data.Hash)
-	if err != nil {
-		return utils.ErrorResponse(c, fiber.StatusBadRequest, "Confiant failed to parse hash value")
+	key := buildKey(data)
+	value, err := buildValue(c, data)
+
+	mod := models.MetadataQueue{
+		Key:           key,
+		TransactionID: bcguid.NewFromf(data.Publisher, data.Domain, time.Now()),
+		Value:         value,
 	}
 
-	err = SendConfiantToRT(context.Background(), *data)
+	err = mod.Insert(c.Context(), bcdb.DB(), boil.Infer())
+
 	if err != nil {
-		return err
+		return utils.ErrorResponse(c, fiber.StatusBadRequest, "Failed to insert metadata update to queue")
 	}
 	return nil
+}
+
+func buildKey(data *ConfiantUpdateRequest) string {
+	key := "confiant:v2:" + data.Publisher
+	if data.Domain != "" {
+		key = key + ":" + data.Domain
+	}
+	return key
+}
+
+func buildValue(c *fiber.Ctx, data *ConfiantUpdateRequest) (types.JSON, error) {
+
+	keyRate := keyRate{
+		Key:  data.Hash,
+		Rate: data.Rate,
+	}
+
+	val, err := json.Marshal(keyRate)
+
+	if err != nil {
+		return nil, utils.ErrorResponse(c, fiber.StatusBadRequest, "Confiant failed to parse hash value")
+	}
+	return val, err
 }
 
 func UpdateConfiant(c *fiber.Ctx, data *ConfiantUpdateRequest) error {
@@ -166,65 +185,7 @@ func UpdateConfiant(c *fiber.Ctx, data *ConfiantUpdateRequest) error {
 	return modConf.Upsert(c.Context(), bcdb.DB(), true, []string{models.ConfiantColumns.PublisherID, models.ConfiantColumns.Domain}, boil.Infer(), boil.Infer())
 }
 
-func SendConfiantToRT(c context.Context, updateRequest ConfiantUpdateRequest) error {
-	const PREFIX string = "confiant:v2"
-	modConfiant, err := confiantQuery(c, updateRequest)
-
-	if err != nil && err != sql.ErrNoRows {
-		return eris.Wrapf(err, "failed to fetch confiants")
-	}
-
-	var finalRules []ConfiantRealtimeRecord
-
-	finalRules = createConfiantsMetadata(modConfiant, finalRules, updateRequest)
-
-	finalOutput := struct {
-		Rules []ConfiantRealtimeRecord `json:"rules"`
-	}{Rules: finalRules}
-
-	value, err := json.Marshal(finalOutput)
-	if err != nil {
-		return eris.Wrap(err, "failed to marshal confiantRT to JSON")
-	}
-
-	key := utils.GetMetadataObject(updateRequest)
-	metadataKey := utils.CreateMetadataKey(key, PREFIX)
-	metadataValue := utils.CreateMetadataObject(updateRequest, metadataKey, value)
-
-	err = metadataValue.Insert(c, bcdb.DB(), boil.Infer())
-	if err != nil {
-		return eris.Wrap(err, "failed to insert metadata record")
-	}
-
-	return nil
-}
-
-func createConfiantsMetadata(modConfiant models.ConfiantSlice, finalRules []ConfiantRealtimeRecord, updateRequest ConfiantUpdateRequest) []ConfiantRealtimeRecord {
-	if len(modConfiant) != 0 {
-		confiants := make(ConfiantSlice, 0)
-		confiants.FromModel(modConfiant)
-
-		for _, confiant := range confiants {
-			rule := ConfiantRealtimeRecord{
-				Rule:       utils.GetFormulaRegex("", confiant.Domain, "", false),
-				ConfiantId: confiant.ConfiantKey,
-			}
-			finalRules = append(finalRules, rule)
-		}
-	}
-
-	newRule := ConfiantRealtimeRecord{
-		Rule:       utils.GetFormulaRegex("", updateRequest.Domain, "", true),
-		ConfiantId: updateRequest.Hash,
-		Rate:       updateRequest.Rate,
-	}
-	finalRules = append(finalRules, newRule)
-	return finalRules
-}
-
-func confiantQuery(c context.Context, updateRequest ConfiantUpdateRequest) (models.ConfiantSlice, error) {
-	modConfiant, err := models.Confiants(
-		models.ConfiantWhere.ConfiantKey.EQ(updateRequest.Hash),
-	).All(c, bcdb.DB())
-	return modConfiant, err
+type keyRate struct {
+	Key  string  `json:"key"`
+	Rate float64 `json:"rate"`
 }
