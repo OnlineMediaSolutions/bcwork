71a5e65f8cf464d311ff7bde30bb21f6e9604a88
ifinkiel <ifinkiel@gmail.com>
2024-09-05 21:41:49 +0300

    fix pixalate bug

--- core/pixalate.go
+++ core/pixalate.go
@@ -120,8 +120,8 @@ type Pixalate struct {
 	PixalateKey string     `boil:"pixalate_key" json:"pixalate_key,omitempty" toml:"pixalate_key" yaml:"pixalate_key"`
 	PublisherID string     `boil:"publisher_id" json:"publisher_id,omitempty" toml:"publisher_id" yaml:"publisher_id"`
 	Domain      string     `boil:"domain" json:"domain,omitempty" toml:"domain" yaml:"domain,omitempty"`
-	Rate        float64    `boil:"rate" json:"rate,omitempty" toml:"rate" yaml:"rate"`
-	Active      bool       `boil:"active" json:"active,omitempty" toml:"active" yaml:"active"`
+	Rate        float64    `boil:"rate" json:"rate" toml:"rate" yaml:"rate"`
+	Active      bool       `boil:"active" json:"active" toml:"active" yaml:"active"`
 	CreatedAt   *time.Time `boil:"created_at" json:"created_at,omitempty" toml:"created_at" yaml:"created_at"`
 	UpdatedAt   *time.Time `boil:"updated_at" json:"updated_at,omitempty" toml:"updated_at" yaml:"updated_at,omitempty"`
 }
