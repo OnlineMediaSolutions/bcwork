7459b7d55b959edc59b47f4f234c48bae60f6432
sonaisrayel <sonai@brightcom.com>
2024-09-15 10:58:34 +0400

    code review changes - Delete default values from migration

--- migrations/008_floor.up.sql
+++ migrations/008_floor.up.sql
@@ -3,15 +3,15 @@ create table floor
 (
     publisher varchar(64) references publisher(publisher_id) not null,
     domain varchar(256) not null,
-    country varchar(64) not null default '',
-    device varchar(64) not null default '',
+    country varchar(64) not null,
+    device varchar(64) not null,
     floor float8 not null default 0,
     created_at timestamp not null,
     updated_at timestamp,
-    rule_id varchar(36) not null default '',
+    rule_id varchar(36) not null,
     demand_partner_id varchar(64) not null default '',
-    browser varchar(64) not null default '',
-    os varchar(64) not null default '',
-    placement_type varchar(64) not null default '',
+    browser varchar(64) not null,
+    os varchar(64) not null,
+    placement_type varchar(64) not null,
     primary key (rule_id)
 );
