0129d736c97d0d6a19436f9a7ef00d9b600a79d3
sonaisrayel <sonai@brightcom.com>
2024-09-16 17:40:01 +0400

    Add bulk floor

--- core/bulk/floor.go
+++ core/bulk/floor.go
@@ -2,26 +2,34 @@ package bulk
 
 import (
 	"database/sql"
+	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"github.com/m6yf/bcwork/bcdb"
+	"github.com/m6yf/bcwork/core"
 	"github.com/m6yf/bcwork/models"
-	"github.com/m6yf/bcwork/utils"
-	"github.com/m6yf/bcwork/utils/bcguid"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/viper"
-	"github.com/volatiletech/sqlboiler/v4/boil"
+	"github.com/volatiletech/sqlboiler/v4/queries"
 	"strconv"
+	"strings"
 	"time"
 )
 
 type FloorUpdateRequest struct {
-	utils.MetadataKey
-	Publisher string  `json:"publisher"`
-	Domain    string  `json:"domain"`
-	Device    string  `json:"device"`
-	Floor     float64 `json:"floor"`
-	Country   string  `json:"country"`
+	RuleId        string  `json:"rule_id"`
+	Publisher     string  `json:"publisher"`
+	Domain        string  `json:"domain"`
+	Device        string  `json:"device"`
+	Floor         float64 `json:"floor"`
+	Country       string  `json:"country"`
+	Browser       string  `json:"browser"`
+	OS            string  `json:"os"`
+	PlacementType string  `json:"placement_type"`
 }
+
+const insert_floor_rule_query = `INSERT INTO floor (rule_id, publisher, domain, country, browser, os, device, placement_type, floor,created_at, updated_at) VALUES `
+
+const floor_on_conflict_query = `ON CONFLICT (rule_id) DO UPDATE SET floor = EXCLUDED.floor, updated_at = NOW()`
 
 func MakeChunksFloor(requests []FloorUpdateRequest) ([][]FloorUpdateRequest, error) {
 	chunkSize := viper.GetInt("api.chunkSize")
@@ -49,17 +57,17 @@ func ProcessChunksFloor(c *fiber.Ctx, chunks [][]FloorUpdateRequest) error {
 	defer tx.Rollback()
 
 	for i, chunk := range chunks {
-		floors, metaDataQueue := prepareDataFloor(chunk)
+		floors := prepareDataFloor(chunk)
 
-		if err := bulkInsertFloor(c, tx, floors); err != nil {
+		if err := bulkInsertFloor(tx, floors); err != nil {
 			log.Error().Err(err).Msgf("failed to process bulk update for floor chunk %d", i)
 			return err
 		}
 
-		if err := BulkInsertMetaDataQueue(c, tx, metaDataQueue); err != nil {
-			log.Error().Err(err).Msgf("failed to process metadata queue for chunk %d", i)
-			return err
-		}
+		//if err := BulkInsertMetaDataQueue(c, tx, metaDataQueue); err != nil {
+		//	log.Error().Err(err).Msgf("failed to process metadata queue for chunk %d", i)
+		//	return err
+		//}
 	}
 
 	if err := tx.Commit(); err != nil {
@@ -72,47 +80,64 @@ func ProcessChunksFloor(c *fiber.Ctx, chunks [][]FloorUpdateRequest) error {
 	return nil
 }
 
-func prepareDataFloor(chunk []FloorUpdateRequest) ([]models.Floor, []models.MetadataQueue) {
+func prepareDataFloor(chunk []FloorUpdateRequest) []models.Floor {
 	var floors []models.Floor
-	var metaDataQueue []models.MetadataQueue
 
 	for _, data := range chunk {
-		floors = append(floors, models.Floor{
-			Publisher: data.Publisher,
-			Domain:    data.Domain,
-			Device:    data.Device,
-			Floor:     data.Floor,
-			Country:   data.Country,
-		})
 
-		metadataKey := utils.MetadataKey{
-			Publisher: data.Publisher,
-			Domain:    data.Domain,
-			Device:    data.Device,
-			Country:   data.Country,
+		floor := core.Floor{
+			Publisher:     data.Publisher,
+			Domain:        data.Domain,
+			Country:       data.Country,
+			Device:        data.Device,
+			Floor:         data.Floor,
+			Browser:       data.Browser,
+			OS:            data.OS,
+			PlacementType: data.PlacementType,
 		}
-		key := utils.CreateMetadataKey(metadataKey, "price:floor")
 
-		metaDataQueue = append(metaDataQueue, models.MetadataQueue{
-			Key:           key,
-			TransactionID: bcguid.NewFromf(data.Publisher, data.Domain, time.Now()),
-			Value:         []byte(strconv.FormatFloat(data.Floor, 'f', 2, 64)),
+		floors = append(floors, models.Floor{
+			Publisher:     floor.Publisher,
+			Domain:        floor.Domain,
+			Device:        floor.Device,
+			Floor:         floor.Floor,
+			Country:       floor.Country,
+			Os:            floor.OS,
+			Browser:       floor.Browser,
+			PlacementType: floor.PlacementType,
+			RuleID:        floor.GetRuleID(),
 		})
 	}
 
-	return floors, metaDataQueue
+	return floors
 }
 
-func bulkInsertFloor(c *fiber.Ctx, tx *sql.Tx, floors []models.Floor) error {
-	columns := []string{"publisher", "domain", "device", "floor", "country", "created_at", "updated_at"}
-	conflictColumns := []string{"publisher", "domain", "device", "country"}
-	updateColumns := []string{"floor = EXCLUDED.floor", "created_at = EXCLUDED.created_at", "updated_at = EXCLUDED.updated_at"}
-
-	var values []interface{}
-	currTime := time.Now().In(boil.GetLocation())
-	for _, floor := range floors {
-		values = append(values, floor.Publisher, floor.Domain, floor.Device, floor.Floor, floor.Country, currTime, currTime)
+func bulkInsertFloor(tx *sql.Tx, floors []models.Floor) error {
+	createAt := time.Now().Format("2006-01-02 15") + ":00:00"
+	values := make([]string, 0)
+	for _, rec := range floors {
+		values = append(values, fmt.Sprintf(`('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s')`,
+			rec.RuleID,
+			rec.Publisher,
+			rec.Domain,
+			rec.Country,
+			rec.Browser,
+			rec.Os,
+			rec.Device,
+			rec.PlacementType,
+			[]byte(strconv.FormatFloat(rec.Floor, 'f', 0, 64)),
+			createAt,
+			createAt))
 	}
 
-	return InsertInBulk(c, tx, "floor", columns, values, conflictColumns, updateColumns)
+	query := fmt.Sprint(insert_floor_rule_query, strings.Join(values, ","))
+	query += fmt.Sprintf(floor_on_conflict_query)
+
+	_, err := queries.Raw(query).Exec(tx)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	return nil
 }
