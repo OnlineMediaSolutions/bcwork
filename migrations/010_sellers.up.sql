44f386b450b3d4e2694566a756638ceeff575d51
sonaisrayel <sonai@brightcom.com>
2024-09-17 18:04:26 +0400

    Add competitors migration

---
+++ migrations/010_sellers.up.sql
@@ -0,0 +1,12 @@
+
+create table competitors
+(
+    competitor_name varchar(100) not null,
+    url TEXT,
+    primary key (competitor_name)
+);
+
+CREATE TABLE competitors (
+                             competitor_name VARCHAR(100) primary key,
+                             url TEXT
+);
