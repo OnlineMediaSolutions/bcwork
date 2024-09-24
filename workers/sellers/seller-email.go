92a3f9017f22565790fa7e1727c9a639c4291614
sonaisrayel <sonai@brightcom.com>
2024-09-24 20:56:16 +0400

    Change email template

--- workers/sellers/seller-email.go
+++ workers/sellers/seller-email.go
@@ -3,58 +3,73 @@ package sellers
 import (
 	"bytes"
 	"github.com/m6yf/bcwork/utils"
-	"strings"
 	"text/template"
 )
-
-type CompetitorData struct {
-	Name       string
-	URL        string
-	Publishers []string
-	Domains    []string
-}
 
 type EmailData struct {
 	Body        string
 	Competitors []CompetitorData
 }
 
+type CompetitorData struct {
+	Name            string
+	URL             string
+	PublisherDomain []PublisherDomain
+}
+
+type PublisherDomain struct {
+	Publisher string
+	Domain    string
+}
+
 func GenerateHTMLTableWithTemplate(competitorsData []CompetitorData, body string) (string, error) {
 	const tpl = `
-	<html>
-		<head>
-			<title>Sellers JSON Updates</title>
-			<style>
-				table { width: 100%; border-collapse: collapse; }
-				th, td { border: 1px solid black; padding: 8px; text-align: left; }
-				th { background-color: #f2f2f2; }
-			</style>
-		</head>
-		<body>
-		   <h3>{{.Body}}</h3>
-			<table>
-				<tr>
-					<th>Competitor Name</th>
-					<th>Competitor URL</th>
-					<th>Added Publishers</th>
-					<th>Added Domains</th>
-				</tr>
-				{{range .CompetitorsData}}
-					<tr>
-						<td>{{.Name}}</td>
-						<td>{{.URL}}</td>
-						<td>{{join .Publishers ", "}}</td>
-						<td>{{join .Domains ", "}}</td>
-					</tr>
-				{{else}}
-					<tr>
-						<td colspan="3">No data available</td>
-					</tr>
-				{{end}}
-			</table>
-		</body>
-	</html>
-	`
+      <html>
+            <head>
+                <title>Sellers JSON Updates</title>
+                <style>
+                    table { width: 100%; border-collapse: collapse; }
+                    th, td { border: 1px solid black; padding: 8px; text-align: left; }
+                    th { background-color: #f2f2f2; }
+                </style>
+            </head>
+            <body>
+                <h3>{{.Body}}</h3>
+                <table>
+                    <tr>
+                        <th>Competitor Name</th>
+                        <th>Competitor URL</th>
+                        <th>Publisher-Domain</th>
+                    </tr>
+                    {{range $index, $competitor := .CompetitorsData}}
+                        {{ $publisherDomainCount := len $competitor.PublisherDomain }}
+                        {{ if eq $publisherDomainCount 1 }}
+                            <tr>
+                                <td>{{$competitor.Name}}</td>
+                                <td>{{$competitor.URL}}</td>
+                                <td>{{range $competitor.PublisherDomain}}{{.Publisher}} - {{.Domain}}{{end}}</td>
+                            </tr>
+                        {{ else }}
+                            {{range $publisherIndex, $publisherDomain := $competitor.PublisherDomain}}
+                                {{ if eq $publisherIndex 0 }}
+                                    <tr>
+                                        <td rowspan="{{$publisherDomainCount}}">{{$competitor.Name}}</td>
+                                        <td rowspan="{{$publisherDomainCount}}">{{$competitor.URL}}</td>
+                                        <td>{{$publisherDomain.Publisher}} - {{$publisherDomain.Domain}}</td>
+                                    </tr>
+                                {{ else }}
+                                    <tr>
+                                        <td>{{$publisherDomain.Publisher}} - {{$publisherDomain.Domain}}</td>
+                                    </tr>
+                                {{ end }}
+                            {{end}}
+                        {{ end }}
+                    {{end}}
+                </table>
+            </body>
+        </html>
+
+`
 	data := struct {
 		Body            string
 		CompetitorsData []CompetitorData
@@ -63,10 +78,7 @@ func GenerateHTMLTableWithTemplate(competitorsData []CompetitorData, body string
 		CompetitorsData: competitorsData,
 	}
 
-	t, err := template.New("emailTemplate").Funcs(template.FuncMap{
-		"join": strings.Join,
-	}).Parse(tpl)
-
+	t, err := template.New("emailTemplate").Parse(tpl)
 	if err != nil {
 		return "", err
 	}
