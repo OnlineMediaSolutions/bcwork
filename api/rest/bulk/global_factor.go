a305e96dda2519bc53ecac8c7988324c798c006f
Denis Sotskov <deniss@onlinemediasolutions.com>
2024-09-25 10:15:40 +0300

    [3345] refactored bulk handlers

--- api/rest/bulk/global_factor.go
+++ api/rest/bulk/global_factor.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/m6yf/bcwork/core/bulk"
 	"github.com/m6yf/bcwork/utils"
+	"github.com/rs/zerolog/log"
 )
 
 type GlobalFactorUpdateResponse struct {
@@ -24,16 +25,13 @@ type GlobalFactorUpdateResponse struct {
 func GlobalFactorBulkPostHandler(c *fiber.Ctx) error {
 	var requests []bulk.GlobalFactorRequest
 	if err := c.BodyParser(&requests); err != nil {
+		log.Error().Err(err).Msg("error parsing request body for global factor bulk update")
 		return utils.ErrorResponse(c, http.StatusBadRequest, "error parsing request body for global factor bulk update", err)
 	}
 
-	chunks, err := bulk.MakeGlobalFactorsChunks(requests)
-	if err != nil {
-		return utils.ErrorResponse(c, http.StatusBadRequest, "failed to create chunks for global factor bulk update", err)
-	}
-
-	if err := bulk.ProcessGlobalFactorsChunks(c, chunks); err != nil {
-		return utils.ErrorResponse(c, http.StatusBadRequest, "failed to process global factor bulk update", err)
+	if err := bulk.BulkInsertGlobalFactors(c.Context(), requests); err != nil {
+		log.Error().Err(err).Msg("failed to process bulk global factor updates")
+		return utils.ErrorResponse(c, http.StatusBadRequest, "failed to process bulk global factor updates", err)
 	}
 
 	return utils.SuccessResponse(c, http.StatusOK, "global factor bulk update successfully processed")
