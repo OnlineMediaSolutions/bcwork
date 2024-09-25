0890bf87dbf564fedd55edef2e0845bd802f01de
Denis Sotskov <deniss@onlinemediasolutions.com>
2024-09-25 10:11:57 +0300

    [3345] refactored dpo bulk

--- core/bulk/dpo.go
+++ core/bulk/dpo.go
@@ -5,27 +5,58 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
-	"github.com/friendsofgo/errors"
-	"github.com/gofiber/fiber/v2"
+	"time"
+
 	"github.com/m6yf/bcwork/bcdb"
+	"github.com/m6yf/bcwork/config"
 	"github.com/m6yf/bcwork/core"
 	"github.com/m6yf/bcwork/models"
+	"github.com/m6yf/bcwork/utils"
 	"github.com/m6yf/bcwork/utils/bcguid"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/viper"
-	"github.com/volatiletech/sqlboiler/v4/queries"
-	strconv "strconv"
-	"strings"
-	"time"
 )
 
-const insert_dpo_rule_query = `INSERT INTO dpo_rule (rule_id, demand_partner_id, publisher, domain, country, browser, os, device_type, placement_type, factor,created_at, updated_at) VALUES `
+func BulkInsertDPO(ctx context.Context, requests []core.DPOUpdateRequest) error {
+	chunks, err := makeChunksDPO(requests)
+	if err != nil {
+		return fmt.Errorf("failed to create chunks for dpos updates: %w", err)
+	}
 
-const on_conflict_query = `ON CONFLICT (rule_id) DO UPDATE SET country = EXCLUDED.country,
-	factor = EXCLUDED.factor, device_type = EXCLUDED.device_type, domain = EXCLUDED.domain, placement_type = EXCLUDED.placement_type, updated_at = EXCLUDED.updated_at`
+	tx, err := bcdb.DB().BeginTx(ctx, nil)
+	if err != nil {
+		return fmt.Errorf("failed to begin transaction: %w", err)
+	}
+	defer tx.Rollback()
 
-func MakeChunksDPO(requests []core.DPOUpdateRequest) ([][]core.DPOUpdateRequest, error) {
-	chunkSize := viper.GetInt("api.chunkSize")
+	for i, chunk := range chunks {
+		dpos, metaDataQueue, err := prepareDPOData(ctx, chunk)
+		if err != nil {
+			log.Error().Err(err).Msgf("failed to prepare dpo data for chunk %d", i)
+			return fmt.Errorf("failed to prepare dpo data for chunk %d: %w", i, err)
+		}
+
+		if err := bulkInsertDPO(ctx, tx, dpos); err != nil {
+			log.Error().Err(err).Msgf("failed to process dpos bulk update for chunk %d", i)
+			return fmt.Errorf("failed to process dpos bulk update for chunk %d: %w", i, err)
+		}
+
+		if err := bulkInsertMetaDataQueue(ctx, tx, metaDataQueue); err != nil {
+			log.Error().Err(err).Msgf("failed to process dpos metadata queue for chunk %d", i)
+			return fmt.Errorf("failed to process dpos metadata queue for chunk %d: %w", i, err)
+		}
+	}
+
+	if err := tx.Commit(); err != nil {
+		log.Error().Err(err).Msg("failed to commit transaction in dpos bulk update")
+		return fmt.Errorf("failed to commit transaction in dpos bulk update: %w", err)
+	}
+
+	return nil
+}
+
+func makeChunksDPO(requests []core.DPOUpdateRequest) ([][]core.DPOUpdateRequest, error) {
+	chunkSize := viper.GetInt(config.APIChunkSizeKey)
 	var chunks [][]core.DPOUpdateRequest
 
 	for i := 0; i < len(requests); i += chunkSize {
@@ -39,83 +70,16 @@ func MakeChunksDPO(requests []core.DPOUpdateRequest) ([][]core.DPOUpdateRequest,
 	return chunks, nil
 }
 
-type DpoSlice []*core.DPOUpdateRequest
-
-func (dp *DpoSlice) FromModel(slice models.DpoSlice) error {
-	for _, mod := range slice {
-		dpo := &core.DPOUpdateRequest{
-			DemandPartner: mod.DemandPartnerID,
-		}
-		*dp = append(*dp, dpo)
-	}
-	return nil
-}
-
-func ProcessChunksDPO(c *fiber.Ctx, chunks [][]core.DPOUpdateRequest) error {
-	errMessage, done := proceedBulkDpo(c, chunks)
-	if done {
-		log.Error().Err(errMessage).Msgf("Failed to process dpo_rule")
-		return errMessage
-	}
-
-	errMessage, done = proceedBulkMetadata(c, chunks)
-	if done {
-		log.Error().Err(errMessage).Msgf("Failed to process dpo metadata queue")
-		return errMessage
-	}
-
-	return nil
-}
-
-func proceedBulkMetadata(c *fiber.Ctx, chunks [][]core.DPOUpdateRequest) (error, bool) {
-	tx, _ := bcdb.DB().BeginTx(c.Context(), nil)
-
-	for i, chunk := range chunks {
-		metaDataQueue := prepareMetadataDPO(chunk, context.Background())
-
-		if err := BulkInsertMetaDataQueue(c, tx, metaDataQueue); err != nil {
-			log.Error().Err(err).Msgf("Failed to process dpo metadata queue for chunk %d", i)
-			return err, true
-		}
-	}
-
-	if err := tx.Commit(); err != nil {
-		log.Error().Err(err).Msg("Failed to commit transaction in DPO bulk update")
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"Error": "Failed to commit DPO transaction",
-		}), true
-	}
-	return nil, false
-}
-
-func proceedBulkDpo(c *fiber.Ctx, chunks [][]core.DPOUpdateRequest) (error, bool) {
-
-	tx, err := bcdb.DB().BeginTx(c.Context(), nil)
+func prepareDPOData(ctx context.Context, chunk []core.DPOUpdateRequest) ([]models.DpoRule, []models.MetadataQueue, error) {
+	metaData, err := prepareDPOMetadata(ctx, chunk)
 	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"Error": "Failed to begin transaction for inserting bulk dpo",
-		}), false
+		return nil, nil, fmt.Errorf("cannot prepare dpo metadata: %w", err)
 	}
 
-	for i, chunk := range chunks {
-		dpos := prepareDataDPO(chunk)
-
-		if err := bulkInsertDPO(tx, dpos); err != nil {
-			log.Error().Err(err).Msgf("Failed to process bulk update for dpos %d", i)
-			return err, true
-		}
-	}
-
-	if err := tx.Commit(); err != nil {
-		log.Error().Err(err).Msg("Failed to commit transaction in DPO bulk update")
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"error": "Failed to commit DPO transaction",
-		}), true
-	}
-	return nil, false
+	return prepareDPO(chunk), metaData, nil
 }
 
-func prepareDataDPO(chunk []core.DPOUpdateRequest) []models.DpoRule {
+func prepareDPO(chunk []core.DPOUpdateRequest) []models.DpoRule {
 	var dpos []models.DpoRule
 
 	for _, data := range chunk {
@@ -137,13 +101,16 @@ func prepareDataDPO(chunk []core.DPOUpdateRequest) []models.DpoRule {
 	return dpos
 }
 
-func prepareMetadataDPO(chunk []core.DPOUpdateRequest, ctx context.Context) []models.MetadataQueue {
+func prepareDPOMetadata(ctx context.Context, chunk []core.DPOUpdateRequest) ([]models.MetadataQueue, error) {
 	var metaDataQueue []models.MetadataQueue
 
 	for _, data := range chunk {
-		modDpos, _ := models.DpoRules(models.DpoRuleWhere.DemandPartnerID.EQ(data.DemandPartner)).All(ctx, bcdb.DB())
+		modDpos, err := models.DpoRules(models.DpoRuleWhere.DemandPartnerID.EQ(data.DemandPartner)).All(ctx, bcdb.DB())
+		if err != nil {
+			return nil, fmt.Errorf("cannot get dpo rules for demand partner id [%v]: %w", data.DemandPartner, err)
+		}
 
-		dpos := make(core.DemandPartnerOptimizationRuleSlice, 0, 0)
+		dpos := make(core.DemandPartnerOptimizationRuleSlice, 0, len(modDpos))
 		dpos.FromModel(modDpos)
 
 		dposRT := core.DpoRT{
@@ -154,52 +121,84 @@ func prepareMetadataDPO(chunk []core.DPOUpdateRequest, ctx context.Context) []mo
 		for _, dpo := range dpos {
 			dposRT.Rules = append(dposRT.Rules, dpo.ToRtRule())
 		}
-
 		dposRT.Rules.Sort()
 
 		b, err := json.Marshal(dposRT)
-
 		if err != nil {
-			fmt.Println("Error marshaling JSON for metadata value:", err)
-			continue
+			return nil, fmt.Errorf("error marshaling JSON for dpo metadata value: %w", err)
 		}
 
 		metaDataQueue = append(metaDataQueue, models.MetadataQueue{
 			TransactionID: bcguid.NewFromf(time.Now()),
-			Key:           "dpo:" + data.DemandPartner,
+			Key:           utils.DPOMetaDataKeyPrefix + ":" + data.DemandPartner,
 			Value:         b,
 		})
 	}
-	return metaDataQueue
+	return metaDataQueue, nil
 }
 
-func bulkInsertDPO(tx *sql.Tx, dpos []models.DpoRule) error {
-	createAt := time.Now().Format("2006-01-02 15") + ":00:00"
-	values := make([]string, 0)
-	for _, rec := range dpos {
-		values = append(values, fmt.Sprintf(`('%s','%s','%s','%s','%s','%s','%s','%s','%s',%s,'%s','%s')`,
-			rec.RuleID,
-			rec.DemandPartnerID,
-			rec.Publisher.String,
-			rec.Domain.String,
-			rec.Country.String,
-			rec.Browser.String,
-			rec.Os.String,
-			rec.DeviceType.String,
-			rec.PlacementType.String,
-			[]byte(strconv.FormatFloat(rec.Factor, 'f', 0, 64)),
-			createAt,
-			createAt))
+func bulkInsertDPO(ctx context.Context, tx *sql.Tx, dpos []models.DpoRule) error {
+	req := prepareBulkInsertDPORequest(dpos)
+
+	return bulkInsert(ctx, tx, req)
+}
+
+func prepareBulkInsertDPORequest(dpos []models.DpoRule) *bulkInsertRequest {
+	req := &bulkInsertRequest{
+		tableName: models.TableNames.DpoRule,
+		columns: []string{
+			models.DpoRuleColumns.RuleID,
+			models.DpoRuleColumns.DemandPartnerID,
+			models.DpoRuleColumns.Publisher,
+			models.DpoRuleColumns.Domain,
+			models.DpoRuleColumns.Country,
+			models.DpoRuleColumns.Browser,
+			models.DpoRuleColumns.Os,
+			models.DpoRuleColumns.DeviceType,
+			models.DpoRuleColumns.PlacementType,
+			models.DpoRuleColumns.Factor,
+			models.DpoRuleColumns.CreatedAt,
+			models.DpoRuleColumns.UpdatedAt,
+		},
+		conflictColumns: []string{
+			models.DpoRuleColumns.RuleID,
+		},
+		updateColumns: []string{
+			models.DpoRuleColumns.Country,
+			models.DpoRuleColumns.Factor,
+			models.DpoRuleColumns.DeviceType,
+			models.DpoRuleColumns.Domain,
+			models.DpoRuleColumns.PlacementType,
+			models.DpoRuleColumns.UpdatedAt,
+		},
+		valueStrings: make([]string, 0, len(dpos)),
 	}
 
-	query := fmt.Sprint(insert_dpo_rule_query, strings.Join(values, ","))
-	query += fmt.Sprintf(on_conflict_query)
+	m := len(req.columns)
+	req.args = make([]interface{}, 0, len(dpos)*m)
 
-	_, err := queries.Raw(query).Exec(tx)
-	if err != nil {
-		tx.Rollback()
-		return errors.Wrapf(err, "Failed to update dpo_rule in bulk transaction")
+	for i, dpo := range dpos {
+		offset := i * m
+		req.valueStrings = append(req.valueStrings,
+			fmt.Sprintf("($%v, $%v, $%v, $%v, $%v, $%v, $%v, $%v, $%v, $%v, $%v, $%v)",
+				offset+1, offset+2, offset+3, offset+4, offset+5, offset+6,
+				offset+7, offset+8, offset+9, offset+10, offset+11, offset+12),
+		)
+		req.args = append(req.args,
+			dpo.RuleID,
+			dpo.DemandPartnerID,
+			dpo.Publisher,
+			dpo.Domain,
+			dpo.Country,
+			dpo.Browser,
+			dpo.Os,
+			dpo.DeviceType,
+			dpo.PlacementType,
+			dpo.Factor,
+			currentTime,
+			currentTime,
+		)
 	}
 
-	return nil
+	return req
 }
