5202640ab4ef1d13929ef9f8de8413668354b616
sonaisrayel <sonai@brightcom.com>
2024-10-21 16:08:11 +0400

    Add last migration for factor

--- migrations/007_factor.up.sql
+++ migrations/007_factor.up.sql
@@ -1,17 +1,19 @@
 
 create table factor
 (
-    publisher varchar(64) references publisher(publisher_id) not null,
-    domain varchar(256) not null,
-    country varchar(64) not null,
-    device varchar(64) not null,
+
+    publisher varchar(64),
+    domain varchar(256),
+    country varchar(64),
+    device varchar(64),
     factor float8 not null default 0,
     created_at timestamp not null,
     updated_at timestamp,
-    rule_id varchar(36) not null,
+    rule_id varchar(36) not null default '',
     demand_partner_id varchar(64) not null default '',
-    browser varchar(64) not null,
-    os varchar(64) not null,
-    placement_type varchar(64) not null,
-    primary key (rule_id)
+    browser varchar(64),
+    os varchar(64),
+    placement_type varchar(64),
+    primary key (publisher, domain, device, country)
+
 );
