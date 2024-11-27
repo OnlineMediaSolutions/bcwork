72b10f1f70ab3fd19152dae25aaeeaa0af923a99
ifinkiel <ifinkiel@gmail.com>
2024-11-27 16:58:20 +0100

    Create a worker that will remove old history data

--- workers/clean_history/worker.go
+++ workers/clean_history/worker.go
@@ -25,6 +25,8 @@ const automationUser = -2
 func (w *Worker) Init(ctx context.Context, conf config.StringMap) error {
 
 	w.DatabaseEnv = conf.GetStringValueWithDefault(config.DBEnvKey, "local_prod")
+	w.skipInitRun, _ = conf.GetBoolValue("skip_init_run")
+
 	err := bcdb.InitDB(w.DatabaseEnv)
 	if err != nil {
 		return eris.Wrapf(err, "failed to initalize DB")
