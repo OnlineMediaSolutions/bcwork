98804baebf5d67e594fea8e64cf7eb1d3b1cad6a
Denis Sotskov <deniss@onlinemediasolutions.com>
2024-12-03 15:24:16 +0200

    [3344] testing migrations using goose

--- migrations/00001_init.sql
+++ migrations/00001_init.sql
@@ -1,11 +1,12 @@
-
-create table report_update
+-- +goose Up
+-- +goose StatementBegin
+create table if not exists report_update
 (
     report    varchar(128) not null primary key,
     update_at timestamp    not null
 );
 
-create table demand_hourly
+create table if not exists demand_hourly
 (
     time         timestamp   not null,
     demand_partner_id varchar(36) not null,
@@ -21,7 +22,7 @@ create table demand_hourly
     primary key (time, publisher_id,demand_partner_id,domain)
 );
 
-create table demand_daily
+create table if not exists demand_daily
 (
     time         timestamp   not null,
     demand_partner_id varchar(36) not null,
@@ -37,30 +38,7 @@ create table demand_daily
     primary key (time, publisher_id,demand_partner_id,domain)
 );
 
-
-create table publisher_hourly
-(
-    time         timestamp   not null,
-    publisher_id varchar(36) not null,
-    domain varchar(256) not null default '-',
-    os varchar(64) not null default '-',
-    country varchar(64) not null default '-',
-    device_type varchar(64) not null default '-',
-    bid_requests int8 not null default 0,
-    bid_responses int8 not null default 0,
-    bid_price_count int8 not null default 0,
-    bid_price_total double precision not null default 0,
-    publisher_impressions  int8 not null default 0,
-    demand_impressions  int8 not null default 0,
-    missed_opportunities  int8 not null default 0,
-    supply_total double precision not null default 0,
-    demand_total double precision not null default 0,
-    demand_partner_fee double precision not null default 0,
-    primary key (time, publisher_id,domain,os,country,device_type)
-
-);
-
-create table publisher_daily
+create table if not exists publisher_hourly
 (
     time         timestamp   not null,
     publisher_id varchar(36) not null,
@@ -81,20 +59,37 @@ create table publisher_daily
     primary key (time, publisher_id,domain,os,country,device_type)
 );
 
+create table if not exists publisher_daily
+(
+    time         timestamp   not null,
+    publisher_id varchar(36) not null,
+    domain varchar(256) not null default '-',
+    os varchar(64) not null default '-',
+    country varchar(64) not null default '-',
+    device_type varchar(64) not null default '-',
+    bid_requests int8 not null default 0,
+    bid_responses int8 not null default 0,
+    bid_price_count int8 not null default 0,
+    bid_price_total double precision not null default 0,
+    publisher_impressions  int8 not null default 0,
+    demand_impressions  int8 not null default 0,
+    missed_opportunities  int8 not null default 0,
+    supply_total double precision not null default 0,
+    demand_total double precision not null default 0,
+    demand_partner_fee double precision not null default 0,
+    primary key (time, publisher_id,domain,os,country,device_type)
+);
 
-create table compass_publisher_tag
+create table if not exists compass_publisher_tag
 (
     id serial primary key,
     publisher_id varchar(36) not null,
     device_type varchar(64) not null,
-    domain varchar(256) not null
+    domain varchar(256) not null,
+    constraint compass_publisher_tag_pk unique (publisher_id, device_type, domain)
 );
-alter table compass_publisher_tag
-    add constraint compass_publisher_tag_pk
-        unique (publisher_id, device_type, domain);
 
-
-create table iiq_testing
+create table if not exists iiq_testing
 (
     time         timestamp   not null,
     demand_partner_id varchar(36)  not null,
@@ -105,7 +100,7 @@ create table iiq_testing
     primary key (time,demand_partner_id)
 );
 
-create table id5_testing
+create table if not exists id5_testing
 (
     time         timestamp   not null,
     demand_partner_id varchar(36)  not null,
@@ -116,11 +111,7 @@ create table id5_testing
     primary key (time,demand_partner_id)
 );
 
-
-
-
-
-create table nb_supply_hourly
+create table if not exists nb_supply_hourly
 (
     time         timestamp   not null,
     publisher_id varchar(36) not null,
@@ -147,8 +138,7 @@ create table nb_supply_hourly
     primary key (time, publisher_id,domain,os,country,device_type,placement_type,size,request_type,payment_type,datacenter)
 );
 
-
-create table nb_demand_hourly
+create table if not exists nb_demand_hourly
 (
     time                     timestamp        not null,
     demand_partner_id        varchar(36)      not null,
@@ -176,38 +166,21 @@ create table nb_demand_hourly
     primary key (time, demand_partner_id, demand_partner_placement_id,publisher_id, domain, os, country, device_type,placement_type,size,request_type,payment_type,datacenter)
 );
 
-create table  demand_partner
+create table if not exists demand_partner
 (
     demand_partner_id varchar(36) not null primary key,
     name varchar(128) not null,
     integration_type varchar(36) not null
 );
 
-create table demand_parnter_placement
+create table if not exists demand_parnter_placement
 (
     demand_partner_placement_id varchar(36) not null primary key,
     demand_partner_id varchar(36) not null references  demand_partner(demand_partner_id) on delete cascade ,
     name varchar(256) not null
 );
 
-
-
-create table revenue_hourly
-(
-    time         timestamp   primary key not null,
-    publisher_impressions  int8 not null default 0,
-    sold_impressions  int8 not null default 0,
-    cost double precision not null default 0,
-    revenue double precision not null default 0,
-    demand_partner_fees double precision not null default 0,
-    missed_opportunities int8 not null default 0,
-    data_fee double precision not null default 0,
-    dp_bid_requests int8 not null default 0
-
-);
-
-
-create table revenue_daily
+create table if not exists revenue_hourly
 (
     time         timestamp   primary key not null,
     publisher_impressions  int8 not null default 0,
@@ -220,10 +193,20 @@ create table revenue_daily
     dp_bid_requests int8 not null default 0
 );
 
+create table if not exists revenue_daily
+(
+    time         timestamp   primary key not null,
+    publisher_impressions  int8 not null default 0,
+    sold_impressions  int8 not null default 0,
+    cost double precision not null default 0,
+    revenue double precision not null default 0,
+    demand_partner_fees double precision not null default 0,
+    missed_opportunities int8 not null default 0,
+    data_fee double precision not null default 0,
+    dp_bid_requests int8 not null default 0
+);
 
-
-
-create table iiq_hourly
+create table if not exists iiq_hourly
 (
     time         timestamp   not null,
     dpid varchar(36)  not null,
@@ -235,8 +218,7 @@ create table iiq_hourly
     primary key (time,dpid,datacenter)
 );
 
-
-create table iiq_daily
+create table if not exists iiq_daily
 (
     time         timestamp   not null,
     dpid varchar(36)  not null,
@@ -248,9 +230,7 @@ create table iiq_daily
     primary key (time,dpid,datacenter)
 );
 
-
-
-create table demand_partner_hourly
+create table if not exists demand_partner_hourly
 (
     time         timestamp   not null,
     demand_partner_id varchar(36) not null,
@@ -260,7 +240,7 @@ create table demand_partner_hourly
     primary key (time,demand_partner_id,domain)
 );
 
-create table demand_partner_daily
+create table if not exists demand_partner_daily
 (
     time         timestamp   not null,
     demand_partner_id varchar(36) not null,
@@ -269,7 +249,29 @@ create table demand_partner_daily
     revenue float8 not null default 0,
     primary key (time,demand_partner_id,domain)
 );
+-- +goose StatementEnd
 
-
-
-
+-- +goose Down
+-- +goose StatementBegin
+drop table report_update;
+drop table demand_hourly;
+drop table demand_daily;
+drop table publisher_hourly;
+drop table publisher_daily;
+alter table compass_publisher_tag
+    drop constraint if exists compass_publisher_tag_pk
+        unique (publisher_id, device_type, domain);
+drop table compass_publisher_tag;
+drop table iiq_testing;
+drop table id5_testing;
+drop table nb_supply_hourly;
+drop table nb_demand_hourly;
+drop table  demand_partner;
+drop table demand_parnter_placement;
+drop table revenue_hourly;
+drop table revenue_daily;
+drop table iiq_hourly;
+drop table iiq_daily;
+drop table demand_partner_hourly;
+drop table demand_partner_daily;
+-- +goose StatementEnd
