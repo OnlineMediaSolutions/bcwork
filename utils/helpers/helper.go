d8373628df547a600d51ff79913d3d77e1ff9cc1
sonaisrayel <sonai@brightcom.com>
2024-12-08 20:47:47 +0400

    Delete unused code

--- utils/helpers/helper.go
+++ utils/helpers/helper.go
@@ -1,31 +1,11 @@
 package helpers
 
 import (
-	"reflect"
 	"sort"
 	"strings"
 
 	"github.com/volatiletech/null/v8"
 )
-
-func ReplaceWildcardValues(data interface{}) {
-	val := reflect.ValueOf(data).Elem()
-	for i := 0; i < val.NumField(); i++ {
-		field := val.Field(i)
-		if field.CanSet() && field.Kind() == reflect.String {
-			if field.String() == "all" {
-				field.SetString("")
-			}
-		}
-	}
-}
-
-func GetStringOrEmpty(ns null.String) string {
-	if ns.Valid {
-		return ns.String
-	}
-	return ""
-}
 
 func GetStringWithDefaultValue(str, defaultValue string) string {
 	if str == "" {
