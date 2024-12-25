cbcd52002e22814f40767583e93cef64d1bcdb04
sonaisrayel <sonai@brightcom.com>
2024-12-25 20:46:01 +0400

    Add dp values from db

--- core/dpo.go
+++ core/dpo.go
@@ -38,19 +38,16 @@ type DemandPartnerOptimizationUpdateResponse struct {
 }
 
 type DPOUpdateRequest struct {
-	RuleId         string  `json:"rule_id"`
-	DemandPartner  string  `json:"demand_partner_id"`
-	Publisher      string  `json:"publisher"`
-	Domain         string  `json:"domain,omitempty"`
-	Country        string  `json:"country,omitempty" validate:"country"`
-	Browser        string  `json:"browser,omitempty" validate:"all"`
-	OS             string  `json:"os,omitempty" validate:"all"`
-	DeviceType     string  `json:"device_type,omitempty"`
-	PlacementType  string  `json:"placement_type,omitempty" validate:"all"`
-	Factor         float64 `json:"factor" validate:"required,gte=0,factorDpo"`
-	Automation     string  `json:"automation"`
-	Threshold      string  `json:"threshold"`
-	AutomationName string  `json:"automation_name"`
+	RuleId        string  `json:"rule_id"`
+	DemandPartner string  `json:"demand_partner_id"`
+	Publisher     string  `json:"publisher"`
+	Domain        string  `json:"domain,omitempty"`
+	Country       string  `json:"country,omitempty" validate:"country"`
+	Browser       string  `json:"browser,omitempty" validate:"all"`
+	OS            string  `json:"os,omitempty" validate:"all"`
+	DeviceType    string  `json:"device_type,omitempty"`
+	PlacementType string  `json:"placement_type,omitempty" validate:"all"`
+	Factor        float64 `json:"factor" validate:"required,gte=0,factorDpo"`
 }
 
 type Dpo struct {
