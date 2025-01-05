82b7912b5c5e9b934bff58267c12d36e20f2856f
Denis Sotskov <deniss@onlinemediasolutions.com>
2025-01-05 09:40:07 +0200

    [3477] added tests for demand partner validation

---
+++ validations/demand_partner_validation_test.go
@@ -0,0 +1,187 @@
+package validations
+
+import (
+	"testing"
+
+	"github.com/m6yf/bcwork/dto"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_validateDemandPartner(t *testing.T) {
+	t.Parallel()
+
+	type args struct {
+		request *dto.DemandPartner
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{
+			name: "valid",
+			args: args{
+				request: &dto.DemandPartner{
+					DemandPartnerID:   "id",
+					DemandPartnerName: "name",
+					DPDomain:          "domain.com",
+					CertificationAuthorityID: func() *string {
+						s := "cert_id"
+						return &s
+					}(),
+					ApprovalProcess: dto.GDocApprovalProcess,
+					DPBlocks:        dto.EmailApprovalProcess,
+					POCName:         "poc_name",
+					POCEmail:        "poc_email",
+					SeatOwnerID: func() *int {
+						n := 1
+						return &n
+					}(),
+					ManagerID: func() *int {
+						n := 1
+						return &n
+					}(),
+					IsInclude:               false,
+					Active:                  true,
+					IsDirect:                true,
+					IsApprovalNeeded:        true,
+					ApprovalBeforeGoingLive: true,
+					IsRequiredForAdsTxt:     true,
+					Score:                   5,
+					Comments: func() *string {
+						s := "comments"
+						return &s
+					}(),
+				},
+			},
+			want: []string{},
+		},
+		{
+			name: "invalid_noRequiredFields",
+			args: args{
+				request: &dto.DemandPartner{
+					DemandPartnerID: "id",
+					CertificationAuthorityID: func() *string {
+						s := "cert_id"
+						return &s
+					}(),
+					ApprovalProcess: dto.GDocApprovalProcess,
+					DPBlocks:        dto.EmailApprovalProcess,
+					POCName:         "poc_name",
+					POCEmail:        "poc_email",
+					SeatOwnerID: func() *int {
+						n := 1
+						return &n
+					}(),
+					IsInclude:               false,
+					Active:                  true,
+					IsDirect:                true,
+					IsApprovalNeeded:        true,
+					ApprovalBeforeGoingLive: true,
+					IsRequiredForAdsTxt:     true,
+					Score:                   5,
+					Comments: func() *string {
+						s := "comments"
+						return &s
+					}(),
+				},
+			},
+			want: []string{
+				"DemandPartnerName is mandatory, validation failed",
+				"DPDomain is mandatory, validation failed",
+				"ManagerID is mandatory, validation failed",
+			},
+		},
+		{
+			name: "invalid_approvalProcessNotFromAllowedList",
+			args: args{
+				request: &dto.DemandPartner{
+					DemandPartnerID:   "id",
+					DemandPartnerName: "name",
+					DPDomain:          "domain.com",
+					CertificationAuthorityID: func() *string {
+						s := "cert_id"
+						return &s
+					}(),
+					ApprovalProcess: "some_approval_process",
+					DPBlocks:        dto.EmailApprovalProcess,
+					POCName:         "poc_name",
+					POCEmail:        "poc_email",
+					SeatOwnerID: func() *int {
+						n := 1
+						return &n
+					}(),
+					ManagerID: func() *int {
+						n := 1
+						return &n
+					}(),
+					IsInclude:               false,
+					Active:                  true,
+					IsDirect:                true,
+					IsApprovalNeeded:        true,
+					ApprovalBeforeGoingLive: true,
+					IsRequiredForAdsTxt:     true,
+					Score:                   5,
+					Comments: func() *string {
+						s := "comments"
+						return &s
+					}(),
+				},
+			},
+			want: []string{
+				approvalProcessErrorMessage,
+			},
+		},
+		{
+			name: "invalid_dpBlocksNotFromAllowedList",
+			args: args{
+				request: &dto.DemandPartner{
+					DemandPartnerID:   "id",
+					DemandPartnerName: "name",
+					DPDomain:          "domain.com",
+					CertificationAuthorityID: func() *string {
+						s := "cert_id"
+						return &s
+					}(),
+					ApprovalProcess: dto.GDocApprovalProcess,
+					DPBlocks:        "some_dp_blocks",
+					POCName:         "poc_name",
+					POCEmail:        "poc_email",
+					SeatOwnerID: func() *int {
+						n := 1
+						return &n
+					}(),
+					ManagerID: func() *int {
+						n := 1
+						return &n
+					}(),
+					IsInclude:               false,
+					Active:                  true,
+					IsDirect:                true,
+					IsApprovalNeeded:        true,
+					ApprovalBeforeGoingLive: true,
+					IsRequiredForAdsTxt:     true,
+					Score:                   5,
+					Comments: func() *string {
+						s := "comments"
+						return &s
+					}(),
+				},
+			},
+			want: []string{
+				dpBlocksErrorMessage,
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			got := validateDemandPartner(tt.args.request)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
