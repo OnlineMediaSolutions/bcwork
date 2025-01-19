572cf1a9f46301cdb0583395a93b43d110a2ffb9
sonaisrayel <sonai@brightcom.com>
2025-01-19 12:20:02 +0400

    Add max threshold and min threshold tests

--- validations/demand_partner_validation_test.go
+++ validations/demand_partner_validation_test.go
@@ -385,6 +385,106 @@ func Test_validateDemandPartner(t *testing.T) {
 			},
 			want: []string{},
 		},
+		{
+			name: "test valid min threshold",
+			args: args{
+				request: &dto.DemandPartner{
+					DemandPartnerID:   "id",
+					DemandPartnerName: "name",
+					DPDomain:          "domain.com",
+					Threshold:         0,
+					Automation:        true,
+					CertificationAuthorityID: func() *string {
+						s := "cert_id"
+						return &s
+					}(),
+					Children: []*dto.DemandPartnerChild{
+						{
+							DPChildName:      "child_name",
+							DPChildDomain:    "childdomain.com",
+							PublisherAccount: "12345",
+						},
+					},
+					Connections: []*dto.DemandPartnerConnection{
+						{PublisherAccount: "abcde"},
+					},
+					ApprovalProcess: dto.GDocApprovalProcess,
+					DPBlocks:        dto.EmailApprovalProcess,
+					POCName:         "poc_name",
+					POCEmail:        "poc_email",
+					SeatOwnerID: func() *int {
+						n := 1
+						return &n
+					}(),
+					ManagerID: func() *int {
+						n := 1
+						return &n
+					}(),
+					IsInclude:               false,
+					Active:                  true,
+					IsDirect:                true,
+					IsApprovalNeeded:        true,
+					ApprovalBeforeGoingLive: true,
+					IsRequiredForAdsTxt:     true,
+					Score:                   5,
+					Comments: func() *string {
+						s := "comments"
+						return &s
+					}(),
+				},
+			},
+			want: []string{},
+		},
+		{
+			name: "test valid max threshold",
+			args: args{
+				request: &dto.DemandPartner{
+					DemandPartnerID:   "id",
+					DemandPartnerName: "name",
+					DPDomain:          "domain.com",
+					Threshold:         0.010,
+					Automation:        true,
+					CertificationAuthorityID: func() *string {
+						s := "cert_id"
+						return &s
+					}(),
+					Children: []*dto.DemandPartnerChild{
+						{
+							DPChildName:      "child_name",
+							DPChildDomain:    "childdomain.com",
+							PublisherAccount: "12345",
+						},
+					},
+					Connections: []*dto.DemandPartnerConnection{
+						{PublisherAccount: "abcde"},
+					},
+					ApprovalProcess: dto.GDocApprovalProcess,
+					DPBlocks:        dto.EmailApprovalProcess,
+					POCName:         "poc_name",
+					POCEmail:        "poc_email",
+					SeatOwnerID: func() *int {
+						n := 1
+						return &n
+					}(),
+					ManagerID: func() *int {
+						n := 1
+						return &n
+					}(),
+					IsInclude:               false,
+					Active:                  true,
+					IsDirect:                true,
+					IsApprovalNeeded:        true,
+					ApprovalBeforeGoingLive: true,
+					IsRequiredForAdsTxt:     true,
+					Score:                   5,
+					Comments: func() *string {
+						s := "comments"
+						return &s
+					}(),
+				},
+			},
+			want: []string{},
+		},
 	}
 
 	for _, tt := range tests {
