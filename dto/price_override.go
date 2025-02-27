7c72a4d69606cb81f2dc3c94d1fb1942031dcfad
ifinkiel <ifinkiel@gmail.com>
2025-02-27 11:02:18 +0200

    fix a bug in API

--- dto/price_override.go
+++ dto/price_override.go
@@ -1,11 +1,14 @@
 package dto
 
+import "time"
+
 type PriceOverrideRequest struct {
 	Domain string `json:"domain"`
 	Ips    []Ips  `json:"ips"`
 }
 
 type Ips struct {
-	IP    string  `json:"ip"`
-	Price float64 `json:"price"`
+	IP    string    `json:"ip"`
+	Date  time.Time `json:"-" swagger:"-"`
+	Price float64   `json:"price"`
 }
